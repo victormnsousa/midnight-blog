4030dda070c4d1c3c1f96c56445e545e1fb2d901
victor_marinho <victor@midnightcreatures.midnightcreatures2>
2026-08-07 14:19:41 -0300

    Estruturacao do blog correta

---
+++ config.lua
@@ -0,0 +1,8 @@
+return {
+	title         = "bossa_tech_blog",
+	baseUrl       = "http://localhost:8000/",
+	languageCode  = "en",
+
+	ignoreFiles   = {"%.lnk$","%.tmp$","^Thumbs%.db$"},
+	ignoreFolders = {"^%."},
+}
